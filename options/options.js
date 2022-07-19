37c0394f07df3508f7340a83cedd8db2186e5c53
Crowdbotics <team@crowdbotics.com>
2022-07-19 05:53:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "twin_babarshop_36279",
+  url: "https://twin_babarshop_36279.botics.co",
+  api: "https://twin_babarshop_36279.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
